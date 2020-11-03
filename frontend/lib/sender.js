3677bcc8b754403762a855ab56ec8a80bbd39b3f
Marek Kwaśny <marekkwasny96@gmail.com>
2020-11-04 00:37:19 +0100

    modified sender

--- frontend/lib/sender.js
+++ frontend/lib/sender.js
@@ -1,4 +1,7 @@
 //Really basic version of sender ready to develop
+//TO RESET NGINX:
+//docker-compose up -d --build
+//docker exec -it innovativeproject-fe-metrics_web_1 nginx -s reload to reload nginx
 
 const xhr = new XMLHttpRequest();
 
