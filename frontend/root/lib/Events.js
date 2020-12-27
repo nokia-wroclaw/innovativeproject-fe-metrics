58e6cdbd434c12862a4a0af82ab57a8d44f5041b
DaDudek <dawiddudekstg@gmail.com>
2020-12-27 19:31:09 +0100

    clean events class

--- frontend/root/lib/Events.js
+++ frontend/root/lib/Events.js
@@ -3,27 +3,21 @@ class Events{
     constructor() {
     }
 
-    catchingEventsLogs(name="log",element,eventsList){
-        for (let event in eventsList){
-            element.addEventListener(
-                event,
-                function (ev){
-                    let tags = {};
-                    for (let property in ev) {
-                        let tagValue = "";
-                        if (typeof ev[property] ==="string"){
-                            tagValue = '"'+ev[property]+'"';
-                            tagValue.replaceAll(" ", "_");
-                        }
-                        else if (typeof ev[property] === "number" || typeof ev[property] ==="boolean"){
-                            tagValue= ev[property]
-                        }
-                        if (tagValue !== ""){
-                            tags[property] = tagValue;
-                        }
-                    }
-                    return new Measurement(name, '"'+ev.type+'"', tags)
-                })
+    catchingEventsLogs(name="log",event){
+        let tags = {};
+        for (let property in ev) {
+            let tagValue = "";
+            if (typeof ev[property] ==="string"){
+                tagValue = '"'+ev[property]+'"';
+                tagValue.replaceAll(" ", "_");
+            }
+            else if (typeof ev[property] === "number" || typeof ev[property] ==="boolean"){
+                tagValue= ev[property]
+            }
+            if (tagValue !== ""){
+                tags[property] = tagValue;
+            }
         }
+            return new Measurement(name, '"'+ev.type+'"', tags)
     }
 }
