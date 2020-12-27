b1c266dd1febbfccbcfdecc84a536775db850229
DaDudek <dawiddudekstg@gmail.com>
2020-12-27 19:47:14 +0100

    added performance class

---
+++ frontend/root/lib/Performance.js
@@ -0,0 +1,33 @@
+class Performance{
+
+    constructor() {
+    }
+
+    getPerformance(){
+        let entries = performance.getEntries();
+        const performanceMeasurements = []
+        for(let i = 0; i < entries.length - 1; i++){
+            let entry = entries[i].toJSON();
+            performanceMeasurements.push(this.preparePerformanceMeasurement(entry));
+        }
+        return performanceMeasurements
+    }
+    preparePerformanceMeasurement(entry,name="performance"){
+        let tags = {};
+        let value = entry.duration;
+        for (let property in entry){
+            if (typeof entry[property] === "object"){
+                tags[property] = '"'+ entry[property].constructor.name + '"';
+            }
+            else if (typeof entry[property] === "string") {
+                let string = '"'+entry[property]+'"';
+                tags[property] = string.replaceAll(" ", "_");
+            }
+            else if (typeof entry[property] === "number" || typeof entry[property] ==="boolean") {
+                tags[property] = entry[property];
+            }
+        }
+        return new Measurement("performance",value,tags)
+    }
+
+}
