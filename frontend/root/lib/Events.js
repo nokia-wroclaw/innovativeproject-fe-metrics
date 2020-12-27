2b5aa8316fc6a360801a4745fe68b6dd2d80b313
DaDudek <dawiddudekstg@gmail.com>
2020-12-27 19:33:38 +0100

    typo

--- frontend/root/lib/Events.js
+++ frontend/root/lib/Events.js
@@ -3,21 +3,21 @@ class Events{
     constructor() {
     }
 
-    catchingEventsLogs(name="log",event){
+    catchingEventsLogs(measurementName="log",event){
         let tags = {};
-        for (let property in ev) {
+        for (let property in event) {
             let tagValue = "";
-            if (typeof ev[property] ==="string"){
-                tagValue = '"'+ev[property]+'"';
+            if (typeof event[property] ==="string"){
+                tagValue = '"'+event[property]+'"';
                 tagValue.replaceAll(" ", "_");
             }
-            else if (typeof ev[property] === "number" || typeof ev[property] ==="boolean"){
-                tagValue= ev[property]
+            else if (typeof event[property] === "number" || typeof event[property] ==="boolean"){
+                tagValue= event[property]
             }
             if (tagValue !== ""){
                 tags[property] = tagValue;
             }
         }
-            return new Measurement(name, '"'+ev.type+'"', tags)
+            return new Measurement(measurementName, '"'+event.type+'"', tags)
     }
 }
