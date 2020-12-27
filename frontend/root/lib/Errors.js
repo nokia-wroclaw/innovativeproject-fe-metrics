794eb9dad78a3d84f683a28728bedf736bdfeab8
DaDudek <dawiddudekstg@gmail.com>
2020-12-27 19:36:24 +0100

    added errors class

---
+++ frontend/root/lib/Errors.js
@@ -0,0 +1,24 @@
+class Errors{
+    constructor() {
+    }
+
+    catchingErrors(measurementName='error', errorEvent) {
+        errorEvent.preventDefault();
+        errorEvent.stopImmediatePropagation();
+        console.log(errorEvent.message);
+        let tags = {};
+        for (let property in errorEvent) {
+            if (typeof errorEvent[property] === "object") {
+                tags[property] = '"' + ev[property].constructor.name + '"';
+            } else if (typeof errorEvent[property] === "string") {
+                let string = '"' + errorEvent[property] + '"';
+                if (!string.includes("[native")) {
+                    tags[property] = string.replaceAll(" ", "_");
+                }
+            } else if (typeof errorEvent[property] === "number" || typeof errorEvent[property] === "boolean") {
+                tags[property] = errorEvent[property];
+            }
+        }
+        return new Measurement(measurementName, '"'+errorEvent.message+'"', tags)
+    }
+}
